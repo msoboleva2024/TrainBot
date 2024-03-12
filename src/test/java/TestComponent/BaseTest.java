f08f7e3ff87a029497d6b66a6bace08445ced351
msobolev <masha2005.88@gmail.com>
2024-03-12 14:51:23 +0100

    changed driver location for linux

--- src/test/java/TestComponent/BaseTest.java
+++ src/test/java/TestComponent/BaseTest.java
@@ -45,7 +45,7 @@ public class BaseTest {
 			ChromeOptions options = new ChromeOptions();
 			
 			//WebDriverManager.chromedriver().setup();
-			System.setProperty("webdriver.chrome.driver", "/home/ec2-user/ chromedriver-linux64");
+			System.setProperty("webdriver.chrome.driver", "/home/ec2-user/chromedriver-linux64/chromedriver");
 			
 			if (browserName.contains("headless")) {
 			options.addArguments("headless");
